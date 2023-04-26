8a09b435069e5b55d6a509cd86d05c660d38796f
proNeel02 <prashantdabe02@gmail.com>
2023-04-26 14:16:52 +0530

    npm Bulid

--- api/index.js
+++ api/index.js
@@ -3,13 +3,12 @@ import cors from "cors";
 import TransactionModal from "./models/transaction.js";
 import mongoose from "mongoose";
 import env from "dotenv";
-
 env.config();
 const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
-
+app.use(express.static("public"));
 mongoose.connect(process.env.MONGO_URL);
 
 app.post("/api/transaction", async (req, res) => {
@@ -17,14 +16,14 @@ app.post("/api/transaction", async (req, res) => {
     const transaction = await TransactionModal.create( { name, price, date, description } );
     res.json(transaction);
 });
-
 app.get('/api/transactions', async (req,res) => {
    const  transactions =  await TransactionModal.find();
    res.json(transactions);
 });
 if(process.env.port){
-  app.listen( process.env.port, () => {
+  app.listen( process.env.port || 5000 , () => {
     console.log("Server is Working!");
   });
 }
+
 export default app;
